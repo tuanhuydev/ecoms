22f9d33a9fb2eb98f75526eccb6c95eba3221e82
Nguyen Tuan Huy <huynt@Nguyens-MacBook-Pro.local>
2022-02-17 14:34:27 +0700

    fix(sidebar): scale down sidebar

--- resources/scripts/components/SideNav/index.tsx
+++ resources/scripts/components/SideNav/index.tsx
@@ -21,7 +21,7 @@ const closedMixin = (theme: Theme): CSSObject => ({
   overflowX: 'hidden',
   width: `calc(${theme.spacing(4)} + 1px)`,
   [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(7)} + 1px)`
+    width: `calc(${theme.spacing(3.5)} + 1px)`
   }
 });
 
