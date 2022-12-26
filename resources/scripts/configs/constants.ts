50d31b6c573ea9f10a3941abac36070f1f98512e
tuanhuydev <tuanhuydev@gmail.com>
2022-12-26 13:58:06 +0700

    fix(enviroment): incorrect environment

--- resources/scripts/configs/constants.ts
+++ resources/scripts/configs/constants.ts
@@ -2,7 +2,11 @@ import { Account, User } from 'scripts/utils/interfaces';
 import { SEVERITY, TASK_STATUS, USER_AVAILABILITY, USER_STATUS } from './enums';
 
 export const IS_DEV_ENV = process.env.APP_ENV === 'local';
+
+
 export const APP_URL = IS_DEV_ENV ? 'http://localhost' : process.env.APP_URL;
+
+console.log({ IS_DEV_ENV, APP_URL });
 
 export const API_URL = `${APP_URL}/api`;
 
